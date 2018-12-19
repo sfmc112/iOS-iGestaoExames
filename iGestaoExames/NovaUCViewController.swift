713d4ccd20479be64a9b76984f907142d90211b2
Sarah Ferreira Mendes da Cunha <a21260876@deis-l15-02.local>
2018-12-19 17:14:48 +0000

    tentei

---
+++ iGestaoExames/NovaUCViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NovaUCViewController.swift
+//  iGestaoExames
+//
+//  Created by Sarah Ferreira Mendes da Cunha on 19/12/2018.
+//  Copyright © 2018 AMov - ISEC. All rights reserved.
+//
+
+import UIKit
+
+class NovaUCViewController: UIViewController {
+
+    @IBOutlet weak var tfNome: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
