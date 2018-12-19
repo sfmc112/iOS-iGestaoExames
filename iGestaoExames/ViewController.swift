79d36acb7f4683248be70e46e8cde35c2997582c
Sarah Ferreira Mendes da Cunha <a21260876@deis-l15-02.local>
2018-12-19 10:20:59 +0000

    Initial Commit

---
+++ iGestaoExames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iGestaoExames
+//
+//  Created by Sarah Ferreira Mendes da Cunha on 19/12/2018.
+//  Copyright © 2018 AMov - ISEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
