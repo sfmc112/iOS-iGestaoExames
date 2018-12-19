29cd08c38df1021c723e472b63a7d2e4b966f071
Sarah Ferreira Mendes da Cunha <a21260876@deis-l15-02.local>
2018-12-19 13:18:14 +0000

    table view controller e view para adicionar uma nova uc

---
+++ iGestaoExames/TableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewCell.swift
+//  iGestaoExames
+//
+//  Created by Sarah Ferreira Mendes da Cunha on 19/12/2018.
+//  Copyright © 2018 AMov - ISEC. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    // MARK: Propriedades
+    
+    @IBOutlet weak var nomeUC: UILabel!
+    @IBOutlet weak var dExameNormal: UILabel!
+    @IBOutlet weak var dExameRecurso: UILabel!
+    @IBOutlet weak var dExamespecial: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iGestaoExames/UnidadeCurricular.swift
@@ -0,0 +1,41 @@
+//
+//  UnidadeCurricular.swift
+//  iGestaoExames
+//
+//  Created by Sarah Ferreira Mendes da Cunha on 19/12/2018.
+//  Copyright © 2018 AMov - ISEC. All rights reserved.
+//
+
+import Foundation
+
+class UnidadeCurricular : Comparable {
+    
+    
+    var nome : String
+    var ano : Int
+    var semestre : Int
+    var dExameNormal : Date
+    var dExameRecurso : Date
+    var dExameEspecial : Date
+    
+    init(nome: String, ano: Int, semestre: Int, dExameNormal: Date, dExameRecurso: Date, dExameEspecial: Date) {
+        self.nome = nome
+        self.ano = ano
+        self.semestre = semestre
+        self.dExameNormal = dExameNormal
+        self.dExameRecurso = dExameRecurso
+        self.dExameEspecial = dExameEspecial
+    }
+    
+    var descricao : String {
+        return "UC: \(nome) - \(ano)º ano \(semestre)º semestre \nExame Normal: \(dExameNormal) Exame Recurso: \(dExameRecurso) Exame Especial: \(dExameEspecial)"
+    }
+    
+    static func < (lhs: UnidadeCurricular, rhs: UnidadeCurricular) -> Bool {
+        return (lhs.ano + lhs.semestre) < (rhs.ano + rhs.semestre)
+    }
+    
+    static func == (lhs: UnidadeCurricular, rhs: UnidadeCurricular) -> Bool {
+        return (lhs.ano + lhs.semestre) == (rhs.ano + rhs.semestre)
+    }
+}
